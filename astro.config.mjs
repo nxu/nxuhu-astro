06eb153584863eed9ab42a265cbfc0498b691356
zsolt <nxu@nxu.hu>
2022-11-25 20:06:49 +0100

    Use astro image plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-import { astroImageTools } from "astro-imagetools";
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import mdx from '@astrojs/mdx';
@@ -7,14 +6,13 @@ import mdx from '@astrojs/mdx';
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({
     mode: "directory"
   }),
-  integrations: [
-    astroImageTools,
-    mdx(),
-    vue(),
-  ]
+  integrations: [mdx(), vue(), image({serviceEntryPoint: '@astrojs/image/sharp'})]
 });
