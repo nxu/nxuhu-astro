ef538e1d49809f43a7e418fe9c71b3f63502e8cf
Zsolt Fekete <nxu@nxu.hu>
2024-11-22 18:00:34 +0100

    ✨ Create index on the sqlite DB

--- cube/schema.sql
+++ cube/schema.sql
@@ -1,7 +1,9 @@
-CREATE TABLE records (
+CREATE TABLE IF NOT EXISTS records (
     session TEXT NOT NULL,
     time REAL NOT NULL,
     ts INTEGER NOT NULL,
     result TEXT NOT NULL,
     PRIMARY KEY (ts)
 );
+
+CREATE INDEX IF NOT EXISTS avg_idx ON records ("session", "result", "time");
